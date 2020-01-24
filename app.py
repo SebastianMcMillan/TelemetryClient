45d3ea38767605e3e9c73fec43c4315fae6b262c
Drake Prebyl <drakeprebyl@gmail.com>
2020-01-23 19:16:51 -0600

    Begin conversion to new database format

--- app.py
+++ app.py
@@ -21,29 +21,8 @@ def index():
 
 @app.route('/realtime', methods=['GET'])
 def realtime():
-	# TODO: Get this from database
-	data = {
-		"BMS": {
-			"Voltage": [" V", 423.5],
-			"Current": [" A", 45.3],
-			"Charge": ["%", 83],
-			"Temperature": ["°C", 38.0],
-		},
-		"Motor": {
-			"Power": [" W", 952.1],
-			"Speed": [" mph", 40.3],
-			"Temperature": ["°C", 47.3],
-		},
-		"GPS": {
-			"Timestamp": ["", "2019-10-28 18:41:37"],
-			"Speed": [" mph", 38.7],
-			"Location": ["", '38.9600781,-95.2468986'], #<a href="https://www.google.com/maps/@38.9600781,-95.2468986,14z" target="_blank">
-			"Altitude": [" ft", 865],
-			"Number of satellites": ["", 5],
-		},
-	}
-	
-	return render_template('realtime.html', data=data)
+	return "NYI"
+	#return render_template('realtime.html', data=data)
 	
 # TODO: Endpoint for AJAX requests to update above data on realtime page
 	
