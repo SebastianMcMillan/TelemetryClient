d3d027b206efdc20b51dfe8de40a5c83fdc1e0e3
dprebyl <drakeprebyl@gmail.com>
2020-01-25 11:28:30 -0600

    Working version with new database format

--- app.py
+++ app.py
@@ -33,12 +33,11 @@ def realtime():
 	
 @app.route('/daily', methods=['GET'])
 def daily():
-	date_raw = request.args.get('date', default="")
 	try:
-		# Verify valid date was provided
-		date = datetime.strptime(date_raw, '%Y-%m-%d')
+		# Check if valid date was provided as GET parameter
+		date = datetime.strptime(request.args.get('date', default=""), '%Y-%m-%d')
 	except ValueError:
-		# Default to today (at midnight)
+		# Default to today (at midnight) if not
 		date = datetime.combine(datetime.today(), datetime.min.time())
 		
 	# Formatted date strings when rendering page and buttons to other dates
@@ -46,77 +45,68 @@ def daily():
 	prev_date_str = (date-timedelta(1)).strftime('%Y-%m-%d')
 	next_date_str = (date+timedelta(1)).strftime('%Y-%m-%d')
 	
+	print("==== " + date_str + " ====" + "="*100)
+	
+	# Determines what each tab/graph should display
 	with open(CLIENT_FORMAT_FILE) as file_handle:
 		tab_format = json.load(file_handle)
-		
+	
+	# Specifies information about each sensor in the database
 	with open(DATABASE_FORMAT_FILE) as file_handle:
 		db_format = json.load(file_handle)
-		
+	
+	
 	# Get the data from the database corresponding to the current date
 	# >= and <= are used instead of == due to time zone issues
 	db_data = db.collection(DATABASE_COLLECTION).where("date", ">=", date).where("date", "<=", date+timedelta(1)).stream()
 	try:
-		readings = next(db_data).to_dict()["telemetry"]
+		readings = next(db_data).to_dict()["telemetry"] # The map within the read document
+		readings = OrderedDict(sorted(readings.items())) # Sort the map
+		print("readings: " + str(readings))
 	except StopIteration:
-		readings = []
-		#return "No data for " + date_str
+		readings = [] # No document for day requested
 	
-	# Tabs array represents each different graph tab visible on the client side
+	
+	graph_data = OrderedDict() # The data rearranged for usage in the render template
+	""" graph_data {
+			"Battery": {
+				"Voltage": {"1579970374000": 423, "1579970379000": 419, ...},
+				"Current": {"1579970374000": 46, "1579970379000": 48, ...},
+				...
+			}, "Solar": ...
+		} """
+	
+	
+	# Tabs array represents each different graph tab the user can look at
 	tabs = []
 	for tab in tab_format:
 		tabs.append(tab)
+		graph_data[tab] = OrderedDict()
 		print("===== TAB: " + tab + " =====")
+		
 		# Loop through every sensor the current tab should show a reading for
 		for sensor_id in tab_format[tab]["lines"]:
+		
 			# Find the info about the sensor
 			sensor = next((item for item in db_format if item["id"] == sensor_id), None)
 			# Ensure the sensor is in the database
 			if sensor is not None and "index" in sensor:
+				graph_data[tab][sensor["name"]] = OrderedDict()
 				print("-- " + str(sensor["index"]) + ": " + sensor_id + " --")
+				
+				
 				# Loop through all the sensor readings for the day being viewed
-				for reading in readings:
-					print(reading + ": " + str(readings[reading][sensor["index"]]))
+				for time, reading in readings.items():
+					# reading = second of current day
+					# readings[reading][sensor["index"]] = the value of the sensor at that second
+					
+					unix = int(date.timestamp() + int(time))*1000 # TODO: Timezone incorrect
+					graph_data[tab][sensor["name"]][unix] = reading[sensor["index"]]
+					print(str(unix) + ": " + str(reading[sensor["index"]]))
 			
 		
+	print("graph_data: " + json.dumps(graph_data))
 	
-	return "Testing" # ========================================================
-	
-	
-	graph_data = {} # The data from the database that will be plotted on each graph
-	graph_labels = {} # The keys for each of the types of data per graph
-	
-	for tab in tabs:
-		graph_data[tab] = OrderedDict()
-		init_labels = False
-		
-		tab_data = db.collection(tab).order_by("time").stream()
-		for entry in tab_data:
-			dict = entry.to_dict()
-			print('{} => {}'.format(entry.id, dict))
-			
-			# Create graph_labels (each piece of data that will be a line on the graph)
-			if init_labels == False:
-				graph_labels[tab] = dict.keys()
-				init_labels = True
-			
-			# Skip entries without time (should not exist)
-			if not "time" in dict: continue
-			
-			# Convert time format from database to UNIX timestamp, then remove from dict
-			timestamp = int(dict["time"].timestamp())*1000
-			del dict["time"]
-			
-			dict.pop('location', None) # TEMPORARY
-			
-			graph_data[tab][timestamp] = dict
-			
-		for k,v in graph_data[tab].items():
-			print("TEST: " + str(k))
-	
-	print("GRAPH LABELS: " + str(graph_labels))
-	
-	# list(data.keys()).sort()
-		
 	return render_template('daily.html', **locals())
 	
 @app.route('/longterm', methods=['GET'])
