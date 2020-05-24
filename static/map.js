b7794d8fbaf87af42135b846e3e3f25ee971fd07
alexgisi <apgisi11@gmail.com>
2020-05-24 17:58:25 -0500

    Implement map display

--- static/map.js
+++ static/map.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // set initial values for time dropdowns
 function set_select(select, value) {
     let options = select.options;
@@ -11,6 +13,7 @@ function set_select(select, value) {
 set_select(document.getElementById("start-time"), starttime.toString());
 set_select(document.getElementById("end-time"), endtime.toString());
 
+
 // verify dropdown values
 function on_select_change() {
     let start = Number(document.getElementById("start-time").value);
@@ -23,20 +26,54 @@ function on_select_change() {
     }
 
 }
-// map
-let map = null;
 
+
+// map
 function draw_map() {
-    let location = {lat: 0, lng: 0};
+    if(location_pairs === null) {
+        document.getElementById("map").style.display = 'none';
+        document.getElementById("no-info").style.display = 'inline';
+        return;
+    }
+
+    // location_pairs: [(lat0, lon0), (lat1, lon1), ...] coordinates: [{lat: lat0, lng: lng0}, ...]
+    let coordinates = pairs_to_coordinates(location_pairs);
 
     // initialize the map
-    map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: location});
+    let map = new google.maps.Map(document.getElementById('map'), {center: coordinates[0]});
 
-    let image = 'images/car.png'; // TODO: Add image for car
+    // place markers at start and end of data, drawing lines to show car movement in between
+    let startmarker = make_marker(coordinates[0], map);
+    startmarker.setTitle("Start");
+    startmarker.setLabel("S");
 
-    // The marker
-    let marker = new google.maps.Marker({position: location, map: map});
-    marker.setPosition(location);
+    let endmarker = make_marker(coordinates[coordinates.length-1], map);
+    endmarker.setTitle("End");
+    endmarker.setLabel("E");
 
-    // TODO:
+    let carpath = new google.maps.Polyline({path: coordinates,
+                                            strokeColor: "#2803fc",
+                                            strokeOpacity: 1.0,
+                                            strokeWeight: 2,
+                                            map: map});
+
+    // zoom to a level that covers coordinates
+    let bounds = new google.maps.LatLngBounds();
+    for(let i=0; i < coordinates.length; i++) {
+        bounds.extend(coordinates[i]);
+    }
+    map.fitBounds(bounds);
+}
+
+function make_marker(coordinate, map) {
+    return new google.maps.Marker({position: coordinate, map: map})
+}
+
+function pairs_to_coordinates(pairs) {
+    let coordinates = [];
+    for(let i=0; i < pairs.length; i++) {
+        coordinates.push({lat: pairs[i][0], lng: pairs[i][1]});
+    }
+
+    return coordinates
 }
