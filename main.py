9c56c8fb03957666e6a9785e6ef9a44956a62990
Sebastian McMillan <22755892+SebastianMcMillan@users.noreply.github.com>
2021-03-15 19:19:07 -0500

    Cancel the timer for the buffer in case of error.

--- main.py
+++ main.py
@@ -121,6 +121,7 @@ def fromCar():
             return "Success, buffer limit reached but data uploaded, buffer cleared", 202
         return "Success, data added to buffer", 202
     except Exception as e:
+        countdownToBufferClear.cancel()
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         print(exc_type, fname, exc_tb.tb_lineno)
