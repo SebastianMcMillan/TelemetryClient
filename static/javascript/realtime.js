02ca4b351466633354829a2caad2c74f70a897fd
Sebastian McMillan <22755892+SebastianMcMillan@users.noreply.github.com>
2021-03-15 19:28:49 -0500

    Fix conflicts

--- static/javascript/realtime.js
+++ static/javascript/realtime.js
@@ -8,152 +8,13 @@ window.chartColors = {
 	grey: 'rgb(201, 203, 207)'
 };
 
-(function(global) {
-	var MONTHS = [
-		'January',
-		'February',
-		'March',
-		'April',
-		'May',
-		'June',
-		'July',
-		'August',
-		'September',
-		'October',
-		'November',
-		'December'
-	];
 
-	var COLORS = [
-		'#4dc9f6',
-		'#f67019',
-		'#f53794',
-		'#537bc4',
-		'#acc236',
-		'#166a8f',
-		'#00a950',
-		'#58595b',
-		'#8549ba'
-	];
+let color = Chart.helpers.color;
 
-	var Samples = global.Samples || (global.Samples = {});
-	var Color = global.Color;
-
-	Samples.utils = {
-		// Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
-		srand: function(seed) {
-			this._seed = seed;
-		},
-
-		rand: function(min, max) {
-			var seed = this._seed;
-			min = min === undefined ? 0 : min;
-			max = max === undefined ? 1 : max;
-			this._seed = (seed * 9301 + 49297) % 233280;
-			return min + (this._seed / 233280) * (max - min);
-		},
-
-		numbers: function(config) {
-			var cfg = config || {};
-			var min = cfg.min || 0;
-			var max = cfg.max || 1;
-			var from = cfg.from || [];
-			var count = cfg.count || 8;
-			var decimals = cfg.decimals || 8;
-			var continuity = cfg.continuity || 1;
-			var dfactor = Math.pow(10, decimals) || 0;
-			var data = [];
-			var i, value;
-
-			for (i = 0; i < count; ++i) {
-				value = (from[i] || 0) + this.rand(min, max);
-				if (this.rand() <= continuity) {
-					data.push(Math.round(dfactor * value) / dfactor);
-				} else {
-					data.push(null);
-				}
-			}
-
-			return data;
-		},
-
-		labels: function(config) {
-			var cfg = config || {};
-			var min = cfg.min || 0;
-			var max = cfg.max || 100;
-			var count = cfg.count || 8;
-			var step = (max - min) / count;
-			var decimals = cfg.decimals || 8;
-			var dfactor = Math.pow(10, decimals) || 0;
-			var prefix = cfg.prefix || '';
-			var values = [];
-			var i;
-
-			for (i = min; i < max; i += step) {
-				values.push(prefix + Math.round(dfactor * i) / dfactor);
-			}
-
-			return values;
-		},
-
-		months: function(config) {
-			var cfg = config || {};
-			var count = cfg.count || 12;
-			var section = cfg.section;
-			var values = [];
-			var i, value;
-
-			for (i = 0; i < count; ++i) {
-				value = MONTHS[Math.ceil(i) % 12];
-				values.push(value.substring(0, section));
-			}
-
-			return values;
-		},
-
-		color: function(index) {
-			return COLORS[index % COLORS.length];
-		},
-
-		transparentize: function(color, opacity) {
-			var alpha = opacity === undefined ? 0.5 : 1 - opacity;
-			return Color(color).alpha(alpha).rgbString();
-		}
-	};
-
-	// DEPRECATED
-	window.randomScalingFactor = function() {
-		return Math.round(Samples.utils.rand(-100, 100));
-	};
-
-	// INITIALIZATION
-
-	Samples.utils.srand(Date.now());
-
-	// Google Analytics
-	/* eslint-disable */
-	if (document.location.hostname.match(/^(www\.)?chartjs\.org$/)) {
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-28909194-3', 'auto');
-		ga('send', 'pageview');
-	}
-	/* eslint-enable */
-
-}(this));
-
-function newDateString(ms) {
-	let timestmp = Date.now()+ms;
-	let mom = moment(timestmp);
-    return mom.toDate();
-}
-
-
-var color = Chart.helpers.color;
-var config = {
-    type: 'bar',
+let canvases = Array.from(document.getElementsByClassName("can"));
+let contexts = canvases.map(x => x.getContext('2d'));
+let charts = contexts.map(x => new Chart(x, {
+    type: 'line',
     data: {
         datasets: [{
             backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
@@ -166,7 +27,6 @@ var config = {
         responsive: true,
         title: {
             display: true,
-            text: 'Chart.js Time Point Data'
         },
 		legend: {
         	display: false,
@@ -181,9 +41,6 @@ var config = {
                     labelString: 'Date'
                 },
                 ticks: {
-                    major: {
-                        fontStyle: 'bold',
-                    },
 					maxRotation: 0,
                 }
             }],
@@ -193,25 +50,15 @@ var config = {
                     display: true,
                     labelString: 'value'
                 },
-				ticks: {
-                	min: -1,
-					max: 1,
-				}
+                ticks: {
+                    beginAtZero: true,
+                }
             }]
         }
     }
-};
+}));
 
-let new_data_queue = [];
-let time_queue = [];
-
-window.onload = function() {
-    var ctx = document.getElementById('canvas').getContext('2d');
-    window.myLine = new Chart(ctx, config);
-
-    setInterval(checkForData, 1000);
-    setInterval(updateChart, 1000);
-};
+setInterval(checkForData, 2000);
 
 
 /*
@@ -219,20 +66,23 @@ Adds data to new_data_queue
  */
 function checkForData() {
     const http = new XMLHttpRequest();
-    const url = "http://127.0.0.1:5000/realtime/give-bool";
+    const url = "http://127.0.0.1:5000/realtime/data";
     http.open("GET", url);
     http.send();
 
     http.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            let bool = parseInt(http.responseText);
-            if (bool) {
-            	new_data_queue.push(1);
-			} else {
-            	new_data_queue.push(-1);
-			}
-
-			time_queue.push(newDateString(0));
+            let data = JSON.parse(http.responseText);
+            console.log(data);
+            for (let key in data) {
+                for(let i = 0; i < charts.length; i++) {
+                    let chart = charts[i];
+                    let parsed_id = chart.canvas.id.split("-")[1];
+                    if(parsed_id === key) {
+                        updateChart(chart, [1000*parseInt(data["gps_time"])], [parseInt(data[key])]);
+                    }
+                }
+            }
         }
     };
 }
@@ -241,9 +91,10 @@ function checkForData() {
 /*
 Updates chart with values from new_data_queue, then deletes values from new_data_queue
  */
-function updateChart() {
-	let data = config.data.datasets[0].data;
+function updateChart(chart, time_queue, new_data_queue) {
+	let data = chart.config.data.datasets[0].data;
 	for (let i=0; i < new_data_queue.length; i++) {
+	    console.log("length is ", new_data_queue.length);
 		if(data.length > 5) {
 			data.splice(0, 1);
 		}
@@ -253,6 +104,17 @@ function updateChart() {
 		});
 		time_queue.splice(i, 1);
 		new_data_queue.splice(i, 1);
-		window.myLine.update();
+		chart.update();
     }
+	updateHead(chart)
+}
+
+
+function updateHead(chart) {
+    let latest_val = chart.config.data.datasets[0].data[chart.config.data.datasets[0].data.length-1].y;
+    let data_key = chart.canvas.id.split("-")[1];
+    let head_key = "head-" + data_key;
+
+    let header = document.getElementById(head_key);
+	header.innerText = header.innerText.split(':')[0] +  ": " + latest_val;
 }
